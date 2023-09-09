fdbccfdb35c300748381ad1b1b21e79a8cd1e678
Tom Doder <tdoder89@gmail.com>
2023-09-09 14:51:03 -0400

    lay down routes to pages

--- src/scenes/global/Navbar.jsx
+++ src/scenes/global/Navbar.jsx
@@ -1,3 +1,12 @@
+import { useDispatch, useSelector } from "react-redux";
+import { Badge, Box, IconButton } from "@mui/material";
+import {
+    PersonOutline,
+    ShoppingBagOutlined,
+    MenuOutlined,
+    SearchOutlined
+} from "@mui/icons-material"
+
 const Navbar = () => {
     return <div>Navbar</div>;
 };
