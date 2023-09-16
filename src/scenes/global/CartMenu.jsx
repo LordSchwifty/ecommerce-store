c322f1a28a981dd88c748614bbcc8c4d55661abb
Tom Doder <tdoder89@gmail.com>
2023-09-16 13:04:46 -0400

    finished functionality and styling of shopping cart

--- src/scenes/global/CartMenu.jsx
+++ src/scenes/global/CartMenu.jsx
@@ -60,14 +60,75 @@ const CartMenu = () => {
               <Box>
                 {cart.map((item) => (
                     <Box key={`${item.attributes.name}-${item.id}`}>
-
+                        <FlexBox p="15px 0">
+                            <Box flex="1 1 40%">
+                                <img
+                                  alt={item?.name}
+                                  width="123px"
+                                  height="164px"
+                                  src={`http://localhost:1337${item?.attributes?.image?.data?.attributes?.formats?.medium?.url}`}
+                                  />
+                            </Box>
+                            <Box flex="1 1 60%">
+                                <FlexBox mb="5px">
+                                    <Typography fontWeight="bold">
+                                        {item.attributes.name}
+                                    </Typography>
+                                    <IconButton onClick={() => dispatch(removeFromCart({ id: item.id}))}>
+                                      <CloseIcon />
+                                    </IconButton>
+                                </FlexBox>
+                                <Typography>{item.attributes.shortDescription}</Typography>
+                                <FlexBox m="15px 0">
+                                    <Box
+                                    display="flex"
+                                    alignItems="center"
+                                    border={`1.5px solid ${shades.neutral[500]}`}>
+                                     <IconButton
+                                        onClick={() => dispatch(decreaseCount({id: item.id}))}
+                                     >
+                                       <RemoveIcon />
+                                     </IconButton>
+                                     <Typography>{item.count}</Typography>
+                                     <IconButton
+                                        onClick={() => dispatch(increaseCount({id: item.id}))}
+                                     >
+                                       <AddIcon />
+                                     </IconButton>
+                                    </Box>
+                                </FlexBox>
+                                <Typography fontWeight="bold">
+                                    ${item.attributes.price}
+                                    </Typography>
+                            </Box>
+                        </FlexBox>
+                        <Divider />
                     </Box>
                 ))}
               </Box>
+              {/* Actions */}
+              <Box m="20px 0">
+                <FlexBox m="20px 0">
+                <Typography fontWeight="bold">SUBTOTAL</Typography>
+                <Typography fontWeight="bold">${totalPrice}</Typography>
+                </FlexBox>
+                <Button
+                sx={{
+                    backgroundColor: shades.primary[400],
+                    color:"white",
+                    borderRadius:0,
+                    minWidth: "100%",
+                    padding: "20px 40px",
+                    m:"20px 0"
+                }}
+                onClick={() => {
+                    navigate("/checkout")
+                    dispatch(setIsCartOpen({}))
+                }}
+                >CHECKOUT</Button>
+              </Box>
             </Box>
-
         </Box>
-
     </Box>
    )
 }
